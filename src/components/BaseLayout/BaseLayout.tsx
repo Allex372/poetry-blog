239eee305d6196f90f932a765f2e0d958e0f4a5c
Allex372 <oleksandr.baran@rolique.io>
2022-10-07 10:38:58 +0300

    fix styles

--- src/components/BaseLayout/BaseLayout.tsx
+++ src/components/BaseLayout/BaseLayout.tsx
@@ -6,13 +6,12 @@ import { Sidebar } from '../../components';
 import styles from './BaseLayout.module.scss';
 
 interface BaseLayoutProps {
-  children: React.ReactNode; // 👈️ type children
+  children: React.ReactNode;
 }
 
 export const BaseLayout: FC<BaseLayoutProps> = ({ children }) => {
-  console.log(children);
   return (
-    <div className="bg-gray flex h-100">
+    <div className={styles.baseLayoutWrapper}>
       <Sidebar />
       <div className="flex flex-column flex-1">
         <div className={clsx(styles.container, 'flex', 'flex-1')}>
