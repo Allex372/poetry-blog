83c7037775b85bf8e999c569a3c1b253970af300
Allex372 <oleksandr.baran@rolique.io>
2022-11-27 12:25:32 +0200

    userPageposts

--- src/pages/PostsLayot/PostCard/PostCard.tsx
+++ src/pages/PostsLayot/PostCard/PostCard.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { Formik, Form, Field } from 'formik';
 import { TextField } from 'formik-material-ui';
 import { toast } from 'react-toastify';
@@ -12,6 +13,7 @@ import clsx from 'clsx';
 import { ThumbsUp, DeleteIcon } from '../../../icons';
 import { Comment } from '../../../types';
 import { useAuth } from '../../../context';
+import { links } from '../../../App';
 
 import styles from './PostCard.module.scss';
 
@@ -125,7 +127,12 @@ export const PostCard = ({
             <p className={clsx(theme && theme === 1 ? [styles.title, styles.titleLight] : styles.title)}>
               Created by:&nbsp;
             </p>
-            <p className={clsx(theme && theme === 1 ? [styles.title, styles.titleLight] : styles.title)}>{userName}</p>
+            <NavLink
+              to={links.ClientAccount({ id: userID })}
+              className={clsx(theme && theme === 1 ? [styles.title, styles.titleLight] : styles.title)}
+            >
+              {userName}
+            </NavLink>
           </div>
           {userID === creator && (
             <div>
